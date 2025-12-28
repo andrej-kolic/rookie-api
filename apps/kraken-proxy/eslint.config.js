073b29a326eb51d6450d64191e386c498fbf4ca8
Rey <andrej.kolic@gmail.com>
2025-12-28 22:38:51 +0100

    cleanup

--- apps/kraken-proxy/eslint.config.js
+++ apps/kraken-proxy/eslint.config.js
@@ -1,5 +1,4 @@
 import { config as expressConfig } from '@repo/eslint-config/express';
-import globals from 'globals';
 
 /** @type {import("eslint").Linter.Config} */
 export default [...expressConfig];
