14cd81b1f05cf02df63d0d4cd9686b3a9d1a41b9
Rey <andrej.kolic@gmail.com>
2025-12-29 12:44:39 +0100

    reenable type check on test files

--- apps/kraken-proxy/eslint.config.js
+++ apps/kraken-proxy/eslint.config.js
@@ -1,12 +1,13 @@
 import {
   config as expressConfig,
-  disableTypeCheck,
+  // disableTypeCheck,
 } from '@repo/eslint-config/express';
 
 /** @type {import("eslint").Linter.Config} */
 export default [
   ...expressConfig,
+
   // Disable strict type checking for test files due to Jest global types
   // This is a common pattern when using strict TypeScript with Jest
-  disableTypeCheck(['**/*.test.ts', '**/*.spec.ts', 'test/**']),
+  // disableTypeCheck(['**/*.test.ts', '**/*.spec.ts', 'test/**']),
 ];
