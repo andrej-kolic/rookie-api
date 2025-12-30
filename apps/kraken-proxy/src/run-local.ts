4e21e7ef3d73e9b1790d3769ca28bf99c85a7cc5
Rey <andrej.kolic@gmail.com>
2025-12-30 14:55:00 +0100

    remove lambda defense for local run

--- apps/kraken-proxy/src/run-local.ts
+++ apps/kraken-proxy/src/run-local.ts
@@ -1,18 +1,9 @@
-// TODO: move to separate file
 import dotenv from 'dotenv';
 import { app } from './app';
 import { config } from './config';
 
-// TODO: move to separate file
-// In Lambda, don't try to load .env file
-if (process.env.AWS_LAMBDA_FUNCTION_NAME === undefined) {
-  dotenv.config();
-}
+dotenv.config();
 
-// TODO: move to separate file
-// Only start server if not running in Lambda
-if (process.env.AWS_LAMBDA_FUNCTION_NAME === undefined) {
-  app.listen(config.port, () => {
-    console.log(`Rookie API listening at http://localhost:${config.port}`);
-  });
-}
+app.listen(config.port, () => {
+  console.log(`Rookie API listening at http://localhost:${config.port}`);
+});
